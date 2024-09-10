0e0decad7cb497e371608fa9e3d59aee5346f3a1
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-09-10 10:26:09 +0700

    add info program is waiting for scheduled

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import cron from "node-cron";
 import { createTalisLoggers } from "./src/talis/index.js";
 import { createNojsLoggers } from "./src/battery/index.js";
 
+console.log("Waiting for scheduled tasks 6 minutes...");
+
 // Schedule tasks to be run on the server every 6 minutes
 cron.schedule("*/6 * * * *", async () => {
   console.log("Running every 6 minutes");
